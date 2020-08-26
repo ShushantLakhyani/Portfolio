03bcb128bfd0714135b3f5a1e04d5d3592c9932f
Shushant Lakhyani <slakhyani20@gmail.com>
2020-08-26 15:33:48 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,86 @@
+$(window).on("load", function() {
+	$(".loader .inner").fadeOut(500, function() {
+		$(".loader").fadeOut(750);
+
+	});
+})
+
+
+
+$(document).ready(function() {
+
+	$('#slides').superslides({
+		animation: 'fade',
+		play: 5000,
+		pagination: false
+	});
+
+	var typed = new Typed(".typed", {
+		strings: ["Software Engineer.", "Web Developer.", "Student."],
+		typeSpeed: 70,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false
+	});
+
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    items: 4,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:2
+	        },
+	        768:{
+	            items:3
+	        },
+	        938:{
+	            items:4
+	        }
+	    }
+	});
+
+
+	$('.chart').easyPieChart({
+        easing: 'easeInOut',
+        barColor: '#fff',
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function(from, to, percent) {
+        	$(this.el).find('.percent').text(Math.round(percent));
+        }
+    });
+
+
+    var skillsTopOffset = $(".skillsSection").offset().top;
+
+	$(window).scroll(function() {
+
+		if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+
+			$('.chart').easyPieChart({
+		        easing: 'easeInOut',
+		        barColor: '#fff',
+		        trackColor: false,
+		        scaleColor: false,
+		        lineWidth: 4,
+		        size: 152,
+		        onStep: function(from, to, percent) {
+		        	$(this.el).find('.percent').text(Math.round(percent));
+		        }
+		    });
+			
+
+		}
+
+
+	});
+
+
+});
+
+
